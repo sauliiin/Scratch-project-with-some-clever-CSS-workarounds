4816c446ee3082dda12cd8eeb06cf7475ab63003
Marcelo Machado <marcelomachadoalves@hotmail.com.br>
2024-04-07 12:23:10 -0300

    responsivo main 02

--- main.js
+++ main.js
@@ -6,7 +6,9 @@ const linksNav = document.querySelector('.links__navigation')
 menuIcon.addEventListener('click', function abrirMenu() {
     if(linksNav.classList.contains('ativo')) {
         linksNav.classList.remove('ativo')
+        document.querySelector('.menu__icon img').src = 'assets/menu-icon.png'
     } else {
         linksNav.classList.add('ativo')
+        document.querySelector('.menu__icon img').src = 'assets/closeX-icon.png'
     }
 })
