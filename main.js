f392a986744751414a6994884aee38989110a362
Master Yoda Rossi <saulohr@yahoo.com.br>
2024-05-03 14:22:43 -0300

    Update main.js

--- main.js
+++ main.js
@@ -12,3 +12,39 @@ menuIcon.addEventListener('click', function abrirMenu() {
         document.querySelector('.menu__icon img').src = 'assets/closeX-icon.png'
     }
 })
+
+
+
+
+let consumo = (X - 24.26)*0.95553117
+
+function custoPainel (consumo) {
+    switch(consumo) {
+        case (consumo > 0 && consumo <= 400):
+            return 15000
+        case (consumo > 400 && consumo <= 600):
+            return 22000
+        case (consumo > 600 && consumo <= 800):
+            return 25000
+        case (consumo > 800 && consumo <= 1000):
+            return 15000
+        default:
+            console.log('ERRO') // Default é pra quando nenhuma das opções anteriores retornou resultado. Se ela estiver no final, nao precisa do break
+    }    
+}
+
+
+
+function economia(contaM) {
+    let consumo = (contaM - 24.26)*0.95553117;
+    let custoP = custoPainel(consumo);
+    let economiaTotal = 0;
+    while (economiaTotal < custoP) {
+      let economiaMes = (contaM * 1.008 ** meses) - 72;
+      economiaTotal += economiaMes;
+      meses++
+    }
+    return meses;
+}
+
+console.log(economia(250, 15000))
