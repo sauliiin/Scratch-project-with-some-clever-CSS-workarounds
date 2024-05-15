ebbc938d4d32b2d8a7057ee6ae65ab8c2d1e9135
Master Yoda Rossi <saulohr@yahoo.com.br>
2024-05-15 10:15:45 -0300

    some code corrections - page F under development

---
+++ Pages/animation.js
@@ -0,0 +1,17 @@
+function play() {
+    vid.play();
+}
+
+
+function playVid() {  
+    document.getElementById("resultados").style.transition = "2s"
+    document.getElementById("resultados").style.backgroundColor = "#ffffff13"
+    document.getElementById("resultados").style.color = "white"
+} 
+
+function pause() { 
+    document.getElementById("resultados").style.transition = "2s"
+    document.getElementById("resultados").style.backgroundColor = "#ffffffff"
+} 
+
+
